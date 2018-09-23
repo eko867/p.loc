e76c696cb59b0a1152ebf6e6b9c110f6bbe45f3c
drive867 <drive867@gmail.com>
2018-09-23 21:42:03 +0300

    home v.0.2.1

--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 use Zend\Mvc\Application;
 use Zend\Stdlib\ArrayUtils;
 
+ini_set('upload_max_filesize', '40M');
+ini_set('post_max_size', '40M');
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
